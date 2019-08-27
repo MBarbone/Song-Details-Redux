10188780c94c5382b811ac3ecb189fbb267725a7
Mike <michael.barbone@gmail.com>
2019-08-27 14:21:15 -0400

    Create Redux store and render list of songs

---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "Smells Like Teen Spirit",
+      duration: "3:35"
+    },
+    {
+      title: "Steal My Sunshine",
+      duration: "3:50"
+    },
+    {
+      title: "All Start",
+      duration: "4:33"
+    },
+    {
+      title: "I Want It That Way",
+      duration: "4:40"
+    }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
